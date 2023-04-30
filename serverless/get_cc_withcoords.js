7c517393a14b38c4750d4731a8b03ae17b938273
Diogo <diogo.peralta.goncalves@gmail.com>
2023-04-30 11:03:43 +0100

    test 1

--- serverless/get_cc_withcoords.js
+++ serverless/get_cc_withcoords.js
@@ -1,31 +1,36 @@
 console.log("hello");
-const fetch = require("node-fetch");
-console.log("hello");
+/* const fetch = require("node-fetch"); */
+
+import('node-fetch').then((fetch) => {
+  // your code using fetch() goes here
 
 
-const { WEATHER_API_KEY } = process.env;
+    console.log("after");
 
-exports.handler = async (event, context) => {
-    const params = JSON.parse(event.body);
-    const { lat, lon, units } = params;
-    const unitts = units === "celsius" ? "metric" : "imperial";
-    const url = `https://api.openweathermap.org/data/2.5/weather?&lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=${unitts}`;
 
-    try {
-        const currentWeatherwithcoords = await fetch(url);
-        const currentweatherJson = await currentWeatherwithcoords.json();
-     return {
-            statusCode: 200,
-            body: JSON.stringify(currentweatherJson)
-        };
-    } catch (error) {
-        console.error(error);
+    const { WEATHER_API_KEY } = process.env;
+
+    exports.handler = async (event, context) => {
+        const params = JSON.parse(event.body);
+        const { lat, lon, units } = params;
+        const unitts = units === "celsius" ? "metric" : "imperial";
+        const url = `https://api.openweathermap.org/data/2.5/weather?&lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=${unitts}`;
+
+        try {
+            const currentWeatherwithcoords = await fetch(url);
+            const currentweatherJson = await currentWeatherwithcoords.json();
         return {
-          statusCode: 500,
-          body: JSON.stringify({ message: 'Internal Server Error' }),
-        };
-      }
-}
-
+                statusCode: 200,
+                body: JSON.stringify(currentweatherJson)
+            };
+        } catch (error) {
+            console.error(error);
+            return {
+              statusCode: 500,
+              body: JSON.stringify({ message: 'Internal Server Error' }),
+            };
+          }
+    }
+});
   
   
