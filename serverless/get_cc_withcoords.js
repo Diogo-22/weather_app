32295a5d956c565cba0c5151d136a80fcc8f3a1c
Diogo <diogo.peralta.goncalves@gmail.com>
2023-04-30 14:29:57 +0100

    console logs

--- serverless/get_cc_withcoords.js
+++ serverless/get_cc_withcoords.js
@@ -1,17 +1,18 @@
-console.log("hello");
+
 const fetch = require("node-fetch");
 
 /* import('node-fetch').then((fetch) => { */
   // your code using fetch() goes here
 /*   import fetch from 'node-fetch' */
 
-
-    console.log("after");
+console.log("hello");
+    
 
 
     const { WEATHER_API_KEY } = process.env;
 
     exports.handler = async (event, context) => {
+      console.log("after");
         const params = JSON.parse(event.body);
         const { lat, lon, units } = params;
         const unitts = units === "celsius" ? "metric" : "imperial";
